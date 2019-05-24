8270d394c9a42f22eabd19429ae0169ce48a3ca2
xiong.bob@gmail.com <xiong.bob@gmail.com>
2019-05-24 14:11:06 -0400

    whatever-commit-exe301Dog

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @Min(1)
+    private String age;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
